207cafef9e2d68ae32b73458bc2c2e96a26aff60
Rafael Aburto <101149365+Hachigud@users.noreply.github.com>
2023-10-03 21:46:07 -0300

    Agrego funcionalidad para exportar facturar en excel, tambien funciona en ingles

---
+++ src/main/java/com/bolsadeideas/springboot/facturito/app/view/xls/FacturaXlsxView.java
@@ -0,0 +1,105 @@
+package com.bolsadeideas.springboot.facturito.app.view.xls;
+
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.bolsadeideas.springboot.facturito.app.models.entity.Factura;
+import com.bolsadeideas.springboot.facturito.app.models.entity.ItemFactura;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("factura/ver.xlsx")
+public class FacturaXlsxView extends AbstractXlsxView{
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		response.setHeader("Content-Disposition", "attachment; filename=\"factura_view.xlsx\"");
+		Factura factura = (Factura) model.get("factura");
+		Sheet sheet = workbook.createSheet("Factura" + " " + factura.getCliente().getNombre());
+		
+		MessageSourceAccessor mensajes = getMessageSourceAccessor();
+		
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0); 
+		
+		cell.setCellValue(mensajes.getMessage("text.factura.ver.datos.cliente"));
+		
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getNombre()+ " " + factura.getCliente().getApellido());
+		
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getEmail());
+		
+		sheet.createRow(4).createCell(0).setCellValue(mensajes.getMessage("text.factura.ver.datos.factura"));
+		sheet.createRow(5).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.folio") + factura.getId());
+		sheet.createRow(6).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.descripcion")+ factura.getDescripcion());
+		sheet.createRow(7).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.fecha") + factura.getCreateAt());
+		
+		CellStyle theaderStyle = workbook.createCellStyle();
+		theaderStyle.setBorderBottom(BorderStyle.MEDIUM);
+		theaderStyle.setBorderTop(BorderStyle.MEDIUM);
+		theaderStyle.setBorderRight(BorderStyle.MEDIUM);
+		theaderStyle.setBorderLeft(BorderStyle.MEDIUM);
+		theaderStyle.setFillForegroundColor(IndexedColors.GOLD.index);
+		theaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		
+		
+		CellStyle theaderBody = workbook.createCellStyle();
+		theaderBody.setBorderBottom(BorderStyle.THIN);
+		theaderBody.setBorderTop(BorderStyle.THIN);
+		theaderBody.setBorderRight(BorderStyle.THIN);
+		theaderBody.setBorderLeft(BorderStyle.THIN);
+		
+		
+		Row header = sheet.createRow(9);
+		header.createCell(0).setCellValue(mensajes.getMessage("text.factura.form.item.nombre"));
+		header.createCell(1).setCellValue(mensajes.getMessage("text.factura.form.item.precio"));
+		header.createCell(2).setCellValue(mensajes.getMessage("text.factura.form.item.cantidad"));
+		header.createCell(3).setCellValue(mensajes.getMessage("text.factura.form.item.total"));
+		header.getCell(0).setCellStyle(theaderStyle);
+		header.getCell(1).setCellStyle(theaderStyle);
+		header.getCell(2).setCellStyle(theaderStyle);
+		header.getCell(3).setCellStyle(theaderStyle);
+		int rownum = 10;
+		for(ItemFactura item: factura.getItems()) {
+			Row fila = sheet.createRow(rownum ++);
+			cell = fila.createCell(0);
+			cell.setCellValue(item.getProducto().getNombre());
+			cell.setCellStyle(theaderBody);
+			
+			cell = fila.createCell(1);
+			cell.setCellValue(item.getProducto().getPrecio());
+			cell.setCellStyle(theaderBody);
+			
+			cell = fila.createCell(2);
+			cell.setCellValue(item.getCantidad());
+			cell.setCellStyle(theaderBody);
+			
+			cell = fila.createCell(3);	
+			cell.setCellValue(item.calcularImporte());
+			cell.setCellStyle(theaderBody);
+		}
+		
+		Row filatotal = sheet.createRow(rownum);
+		filatotal.createCell(2).setCellValue(mensajes.getMessage("text.factura.form.total"));
+		filatotal.createCell(3).setCellValue(factura.getTotal());
+		
+	}
+
+}
